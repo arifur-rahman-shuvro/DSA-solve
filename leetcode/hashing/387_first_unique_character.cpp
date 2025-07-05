1ad752ad1761f34e114d70da1dd5e3d1f94ba8e4
arifur-rahman-shuvro <arifurrahmanshuvroars@gmail.com>
2025-07-05 21:21:09 +0600

    Leetcode 387 Solved(Updated Comment

--- leetcode/hashing/387_first_unique_character.cpp
+++ leetcode/hashing/387_first_unique_character.cpp
@@ -1,41 +1,49 @@
 /*
+Problem: Leetcode 387 - First Unique Character in a String
 Author: Arifur Rahman Shuvro
 Date: 04 July, 2025
 GitHub: https://github.com/arifur-rahman-shuvro
+
+Description:
+Given a string s, return the index of the first non-repeating character.
+If no such character exists, return -1.
+
+Examples:
+Input: leetcode
+Output: 0
+
+Input: loveleetcode
+Output: 2
+
+Time Complexity: O(n)
+Space Complexity: O(1) [Fixed 26-char array]
 */
 
-//#include<bits/stdc++.h>
-#include<iostream>
-#include<string>
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
 using namespace std;
 
 class Solution {
 public:
     int firstUniqChar(const string& s) {
         int hash[26] = {0};
-        for(size_t i = 0; i < s.size(); i++){
+        for (size_t i = 0; i < s.size(); ++i) {
             hash[s[i] - 'a']++;
         }
-        for(int i = 0; i< (int)s.size();i++){
-            if(hash[s[i]-'a'] == 1) return i;
-            
+
+        for (int i = 0; i < (int)s.size(); ++i) {
+            if (hash[s[i] - 'a'] == 1) return i;
         }
+
         return -1;
-        
     }
 };
 
-int main(){
+int main() {
     Solution sol;
     string s;
     getline(cin, s);
-    cout << sol.firstUniqChar(s);
+    cout << sol.firstUniqChar(s) << endl;
+    return 0;
 }
-
-/*
-Input: leetcode
-Output: 1
-
-Input: loveleetcode
-Output: 2
-*/
