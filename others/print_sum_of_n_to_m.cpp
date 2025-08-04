fd6a709b8dfc85fd21059ca119d4468eb0f7eccb
arifur-rahman-shuvro <arifurrahmanshuvroars@gmail.com>
2025-08-04 11:27:07 +0600

    Sum of N to M printed

---
+++ others/print_sum_of_n_to_m.cpp
@@ -0,0 +1,38 @@
+/*
+╔══════════════════════════════════════════════════════════════════╗
+║ Problem    : Leetcode 1 - Two Sum                                ║
+║ Author     : Arifur Rahman Shuvro                                ║
+║ Date       : 30 July, 2025                                       ║
+║ Github     : https://github.com/arifur-rahman-shuvro             ║
+╚══════════════════════════════════════════════════════════════════╝
+*/
+
+/*
+Description: 
+Recursion is used instead of a loop to get sum of n to m (inclusive).
+*/
+
+#include <iostream>
+using namespace std;
+
+// Recursive function to print numbers from n to m
+int sum_n_to_m(int n, int m) {
+    if (n > m) return 0;       // Base case: if n exceeds m, stop recursion
+    return n + sum_n_to_m(n + 1, m);  // Recursive call with n incremented
+    
+}
+
+int main() {
+    int n; // Start point
+    int m; // End point
+    cin >> n >> m;
+    cout << sum_n_to_m(n, m);
+    return 0;
+}
+
+/*
+Input:
+1 10
+
+Output: 55
+*/
