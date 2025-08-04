58fde4445f109480e9562c528d682eb46e615cd4
arifur-rahman-shuvro <arifurrahmanshuvroars@gmail.com>
2025-08-04 11:47:04 +0600

    Leetcode 121 Solved

---
+++ leetcode/array/121_buy_and_sell_stock.cpp
@@ -0,0 +1,41 @@
+/*
+╔══════════════════════════════════════════════════════════════════════════════╗
+║ Problem    : LeetCode 121 - Best Time to Buy and Sell Stock                  ║
+║ Author     : Arifur Rahman Shuvro                                            ║
+║ Date       : 04 August, 2025                                                 ║
+║ GitHub     : https://github.com/arifur-rahman-shuvro                         ║
+╚══════════════════════════════════════════════════════════════════════════════╝
+
+Description:
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0
+
+Example:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Time Complexity  : O(n)  
+Space Complexity : O(1)
+*/
+
+//#include<bits/stdc++.h>
+#include <vector>
+#include <algorithm>
+
+int maxProfit(const std::vector<int>& price) {
+    int n = price.size();
+    int maxProfit = 0;
+    int minCost = price[0];
+    for (int i = 1; i < n; i++) {
+        int profit = price[i] - minCost;
+        maxProfit = std::max(profit, maxProfit);
+        minCost = std::min(minCost, price[i]);
+    }
+    return maxProfit;
+}
